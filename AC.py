ee55a78daa2db9cc042fe515a5853ea42a5734ce
Ariya Gowda <ariyag@uci.edu>
2024-09-02 00:50:42 -0700

    created an AC class

---
+++ AC.py
@@ -0,0 +1,22 @@
+class AC:
+    def __init__(self, access_points):
+        self.access_points = access_points
+        self.memory = []
+
+    def change_channel(self):
+        for a_point in self.access_points:
+            conflicting_aps = [other_ap for other_ap in self.access_points if
+                               other_ap.channel == a.channel and other_ap != ap]
+            if conflicting_aps:
+                preferred_channels = [11, 6, 1]
+                for channel in preferred_channels:
+                    if not any(ap.channel == channel for ap in self.access_points):
+                        a_point.adjust_channel(channel)
+                        break
+                else:
+                    a_point.adjust_channel(a_point.channel - 1 if a_point.channel > 1 else 2)
+
+    def change_memory(self):
+        for a_point in self.access_points:
+            self.memory.extend(a_point.get_memory())
+        return self.memory
