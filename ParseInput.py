4888ad330fd276e436a981eb06ec1f26fa6ece01
Ariya Gowda <ariyag@uci.edu>
2024-09-02 00:33:47 -0700

    created access point class

--- ParseInput.py
+++ ParseInput.py
@@ -1,6 +1,10 @@
+
+
+
 class ParseInput:
 
     def __init__(self):
+        self.access_points = []
 
     def input_parse(self, filename):
         with open(filename, 'r') as file:
@@ -18,6 +22,24 @@ class ParseInput:
                     self.execute_move(inputs)
 
     def execute_ap(self, inputs):
+        e_ap = AccessPoint(
+            ap_name = inputs[1],
+            x = int(inputs[2]),
+            y = int(inputs[3]),
+            channel=int(inputs[4]),
+            power_level=int(inputs[5]),
+            frequency=float(inputs[6]),
+            standard=inputs[7],
+            supports_11k=inputs[8].lower() == 'true',
+            supports_11v=inputs[9].lower() == 'true',
+            supports_11r=inputs[10].lower() == 'true',
+            coverage_radius=int(inputs[11]),
+            device_limit=int(inputs[12]),
+            minimal_rssi=int(inputs[13]) if len(inputs) > 13 else 0
+        )
+        self.access_points.append(e_ap)
+
+
 
     def execute_client(self, inputs):
 
